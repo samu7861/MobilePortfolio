6ff70125b03d9d58297465d7a8f67c182411e759
samu7861 <sfrancog7861@gmail.com>
2024-05-27 11:34:17 -0500

    glassmorphism

--- my_portfolio/lib/Presentation/Screens/proyects/third_page.dart
+++ my_portfolio/lib/Presentation/Screens/proyects/third_page.dart
@@ -1,24 +1,29 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:glassmorphism/glassmorphism.dart';
 
 class ThirdPage extends StatelessWidget {
   const ThirdPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    return Container(
       height: 768,
       width: double.infinity,
-      child: Center(
-        child: GlassMorphism(),
-      ),
-      );
+      alignment: Alignment.center,
+      child: const Wrap(
+        spacing: 20,
+        runSpacing: 20,
+        children: [
+          GlassMorphism(),
+          GlassMorphism(),
+          GlassMorphism(),
+          GlassMorphism(),
+        ],
+      )
+    );
   }
 }
-
-
-
-
 
 class GlassMorphism extends StatelessWidget {
   const GlassMorphism({
@@ -27,30 +32,49 @@ class GlassMorphism extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GlassmorphicContainer(
-        width: 300,
-        height: 500,
-        borderRadius: 0,
-        blur: 20,
-        alignment: Alignment.center,
-        border: 2,
-        linearGradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            const Color(0xFFffffff).withOpacity(0.1),
-            const Color(0xFFFFFFFF).withOpacity(0.05),
-          ],
-        ),
-        borderGradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            const Color(0xFFffffff).withOpacity(0.5),
-            const Color((0xFFFFFFFF)).withOpacity(0.5),
-          ],
-        ),
-    
-      );
+    return ClipRRect(
+    borderRadius: BorderRadius.circular(30),
+    child: Container(
+      width: 300,
+      height: 500,
+      color: Colors.transparent,
+      child: Stack(
+        children: [
+          BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 4.0,
+              sigmaY: 4.0,
+            ),
+            child: Container(),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              border: Border.all(color: Colors.white.withOpacity(0.13)),
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [  
+                    Colors.white.withOpacity(0.15),           
+                    Colors.white.withOpacity(0.05),
+                  ]),
+            ),
+          ),
+          const Center(
+            child: Text(
+              "GlassMorphism", 
+              style: TextStyle(
+                fontSize: 30, color: Colors.white
+                ),
+              )
+            ),
+        ],
+      ),
+    ),
+        );
   }
 }
+
+
+
+
